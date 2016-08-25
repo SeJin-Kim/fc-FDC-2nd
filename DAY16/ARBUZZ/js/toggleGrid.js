f48a23eb1f00dacad85e88ddea440dc2525ab2da
Jee Hoon <yamoo9@naver.com>
2016-08-25 20:27:22 +0900

    17일차 첫시간 진행 내용

--- DAY16/ARBUZZ/js/toggleGrid.js
+++ DAY16/ARBUZZ/js/toggleGrid.js
@@ -4,11 +4,13 @@
   var body = document.body;
   var containers = document.querySelectorAll('.container');
   document.onkeydown = function(event) {
-    if(event.keyCode === 71 && event.shiftKey) {
+    if( event === true || event.keyCode === 71 && event.shiftKey) {
       body.classList.toggle('show-leading');
       Array.prototype.forEach.call(containers, function(container) {
         container.classList.toggle('show-grid');
       });
     }
   };
+
+
 })(this);
